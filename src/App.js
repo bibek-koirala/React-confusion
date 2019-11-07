c92a6dd8a3f1727e3651dc295d62ef89d64518ba
Bibek Koirala <bibek.high@gmail.com>
2019-11-08 01:35:51 +0530

    Component-part2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React,{ Component } from 'react';
 import { Navbar,NavbarBrand } from 'reactstrap';
 import './App.css';
 import Menu from './components/menu-component';
+import { Dishes } from './Shared/dishes'
 
-function App() {
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state={
+      dishes:Dishes
+    };
+  }
+  render(){
   return (
     <div>
       <Navbar dark color="primary">
@@ -11,9 +20,9 @@ function App() {
         <NavbarBrand href="/">So much confusion</NavbarBrand>
       </div>
       </Navbar>
-      <Menu />
+      <Menu dishes={this.state.dishes} />
     </div>
-  );
+  );}
 }
 
 export default App;
